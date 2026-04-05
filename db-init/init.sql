4204aa6b57b52898b6172122b462db679853b304
AkashNegi1 <akashnegi977@gmail.com>
2026-04-05 11:14:46 +0530

    Fix ESM import for Railway deployment

--- db-init/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS traffic_data (
-  id SERIAL PRIMARY KEY,
-  timestamp TIMESTAMP NOT NULL,
-  origin TEXT NOT NULL,
-  destination TEXT NOT NULL,
-  duration_normal INT,
-  duration_in_traffic INT
-);
